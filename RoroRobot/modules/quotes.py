255d3e8d5c538edf1315afd8397e6b371ff26211
KAKASHI-HATAKE1 <116017913+KAKASHI-HATAKE1@users.noreply.github.com>
2022-10-21 00:20:47 +0530

    Update quotes.py

--- RoroRobot/modules/quotes.py
+++ RoroRobot/modules/quotes.py
@@ -1,75 +1,129 @@
-## Made By @Yash-Sharma-1807
-## telegram.dog/YASH_SHARMA_1807?start=papa
+"""
+BSD 2-Clause License
+
+Copyright (C) 2017-2019, Paul Larsen
+Copyright (C) 2021-2022, Awesome-RJ, [ https://github.com/Awesome-RJ ]
+Copyright (c) 2021-2022, Yūki • Black Knights Union, [ https://github.com/Awesome-RJ/CutiepiiRobot ]
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+from io import BytesIO
+from traceback import format_exc
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from RoroRobot.utils.errors import capture_err
+from RoroRobot import arq, pgram, LOGGER
+
+Cutiepii_PYRO_Q = filters.command(["quote", "q"])
 
 
+async def quotify(messages: list):
+    response = await arq.quotly(messages)
+    if not response.ok:
+        return [False, response.result]
+    sticker = response.result
+    sticker = BytesIO(sticker)
+    sticker.name = "sticker.webp"
+    return [True, sticker]
 
-import os
-import time
-from datetime import datetime as dt
-from random import choice
-from shutil import rmtree
-from RoroRobot import quotly
-from RoroRobot.services.quotehelper import eor
-from RoroRobot.events import register  
 
-@register(pattern="^/q(?: |$)(.*)")
-async def quott_(event):
-    match = event.pattern_match.group(1).strip()
-    if not event.is_reply:
-        return await event.eor("Please reply to a message")
-    msg = await event.reply("Creating quote plaese wait")
-    reply = await event.get_reply_message()
-    replied_to, reply_ = None, None
-    if match:
-        spli_ = match.split(maxsplit=1)
-        if (spli_[0] in ["r", "reply"]) or (
-            spli_[0].isdigit() and int(spli_[0]) in range(1, 21)
-        ):
-            if spli_[0].isdigit():
-                if not event.client._bot:
-                    reply_ = await event.client.get_messages(
-                        event.chat_id,
-                        min_id=event.reply_to_msg_id - 1,
-                        reverse=True,
-                        limit=int(spli_[0]),
-                    )
-                else:
-                    id_ = reply.id
-                    reply_ = []
-                    for msg_ in range(id_, id_ + int(spli_[0])):
-                        msh = await event.client.get_messages(event.chat_id, ids=msg_)
-                        if msh:
-                            reply_.append(msh)
-            else:
-                replied_to = await reply.get_reply_message()
-            try:
-                match = spli_[1]
-            except IndexError:
-                match = None
-    user = None
-    if not reply_:
-        reply_ = reply
-    if match:
-        match = match.split(maxsplit=1)
-    if match:
-        if match[0].startswith("@") or match[0].isdigit():
-            try:
-                match_ = await event.client.parse_id(match[0])
-                user = await event.client.get_entity(match_)
-            except ValueError:
-                pass
-            match = match[1] if len(match) == 2 else None
-        else:
-            match = match[0]
-    if match == "random":
-        match = choice(all_col)
+def getArg(message: Message) -> str:
+    arg = message.text.strip().split(None, 1)[1].strip()
+    return arg
+
+
+def isArgInt(message: Message) -> list:
+    count = getArg(message)
     try:
-        file = await quotly.create_quotly(
-            reply_, bg=match, reply=replied_to, sender=user
-        )
-    except Exception as er:
-        return await msg.edit(str(er))
-    message = await reply.reply("", file=file)
-    os.remove(file)
-    await msg.delete()
-    return message
+        count = int(count)
+        return [True, count]
+    except ValueError:
+        return [False, 0]
+
+
+@pgram.on_message(Cutiepii_PYRO_Q & ~filters.forwarded & ~filters.bot)
+@pgram.on_edited_message(Cutiepii_PYRO_Q)
+@capture_err
+async def quote(client, message: Message):
+    await message.delete()
+    if not message.reply_to_message:
+        return await message.reply_text("Reply to a message to quote it.")
+    if not message.reply_to_message.text:
+        return await message.reply_text(
+            "Replied message has no text, can't quote it.")
+    m = await message.reply_text("Quoting Messages Please wait....")
+    if len(message.command) < 2:
+        messages = [message.reply_to_message]
+    elif len(message.command) == 2:
+        arg = isArgInt(message)
+        if arg[0]:
+            if arg[1] < 2 or arg[1] > 10:
+                return await m.edit("Argument must be between 2-10.")
+            count = arg[1]
+            messages = [
+                i for i in await client.get_messages(
+                    message.chat.id,
+                    range(message.reply_to_message.id,
+                          message.reply_to_message.id + (count + 5)),
+                    replies=0) if not i.empty and not i.media
+            ]
+            messages = messages[:count]
+        else:
+            if getArg(message) != "r":
+                return await m.edit(
+                    "Incorrect Argument, Pass **'r'** or **'INT'**, **EX:** __/q 2__"
+                )
+            reply_message = await client.get_messages(
+                message.chat.id,
+                message.reply_to_message.id,
+                replies=1,
+            )
+            messages = [reply_message]
+    else:
+        return await m.edit(
+            "Incorrect argument, check quotly module in help section.")
+    try:
+        if not message:
+            return await m.edit("Something went wrong.")
+        sticker = await quotify(messages)
+        if not sticker[0]:
+            await message.reply_text(sticker[1])
+            return await m.delete()
+        sticker = sticker[1]
+        await message.reply_sticker(sticker)
+        await m.delete()
+        sticker.close()
+    except Exception as e:
+        await m.edit("Something went wrong while quoting messages," +
+                     " This error usually happens when there's a " +
+                     " message containing something other than text," +
+                     " or one of the messages in-between are deleted.")
+        e = format_exc()
+        LOGGER.debug(e)
+
+
+__mod_name__ = "Quotly"
