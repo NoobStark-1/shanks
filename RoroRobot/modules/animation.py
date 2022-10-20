f8a178cd87d0e4689e66dc2cc66b181be1654d3e
XoRishu <115208147+XoRishu@users.noreply.github.com>
2022-10-20 23:13:22 +0530

    Update animation.py

--- RoroRobot/modules/animation.py
+++ RoroRobot/modules/animation.py
@@ -293,7 +293,7 @@ dispatcher.add_handler(MOONANIMATION_HANDLER)
 dispatcher.add_handler(CLOCKANIMATION_HANDLER)
 dispatcher.add_handler(BLOCKANIMATION_HANDLER)
 
-__mod_name__ = "Animation"
+__mod_name__ = "𝙰ɴɪᴍᴀᴛɪᴏɴ"
 __command_list__ = [
     "love",
     "hack",
